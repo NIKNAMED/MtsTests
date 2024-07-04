909b17382ef6a92b22a94b82ba3b3642d0ddb3c5
nikita <niknamed300@mail.ru>
2024-07-04 19:13:16 +0300

    yes

--- src/main/java/Locators.java
+++ src/main/java/Locators.java
@@ -1,7 +1,22 @@
 public class Locators {
-    static final String BLOCK_NAME = "//*[@id=pay-section]/div/div/div[2]/section/div/h2";
+    static final String BLOCK_NAME = "//*[@id='pay-section']/div/div/div[2]/section/div/h2";
 
     static final String PARTNERS_LOGOS = "//div[@class=pay__partners]";
 
     static final String MORE_INFO_BUTTON = "//a[@href=/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/]";
+
+    static final String CHECK_INFO_AFTER_BUTTON_MORE_INFO = "//div[4][@class=container-fluid]";
+
+    static final String INPUT_NUMBER = "//input[@id='connection-phone']";
+
+    static final String INPUT_SUMMA = "//input[@id='connection-sum']";
+
+    static final String INPUT_EMAIL  = "//input[@id='connection-email']";
+
+    static final String BUTTON_CON = "//button[@class='button button__default']";
+
+    static final String CARD_INFO = "#//div[@class='app-wrapper__content-container" +
+            " app-wrapper__content-container_full']";
+
+    static final String SERVICE_NAME = "//span[@class='select__now']";
 }
--- src/test/java/MtsByTests.java
+++ src/test/java/MtsByTests.java
@@ -4,11 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.sql.Driver;
 
 public class MtsByTests {
 
@@ -26,25 +22,37 @@ public class MtsByTests {
     }
 
     @Test
-    @DisplayName("22")
-    public void check() {
+    @DisplayName("1. Проверить название указанного блока;" +
+                 "2. Проверить наличие логотипов платёжных систем;")
+    public void firstTest() {
     driver.findElement(By.xpath(Locators.BLOCK_NAME));
     driver.findElement(By.xpath(Locators.PARTNERS_LOGOS));
+
     }
 
+    @Test
+    @DisplayName("3. Проверить работу ссылки «Подробнее о сервисе»;")
+    public void secondTest() {
+    driver.findElement(By.xpath(Locators.MORE_INFO_BUTTON)).click();
+    driver.findElement(By.xpath(Locators.CHECK_INFO_AFTER_BUTTON_MORE_INFO)).isDisplayed();
 
+    }
 
+    @Test
+    @DisplayName("4. Заполнить поля и проверить работу кнопки «Продолжить» " +
+            "(проверяем только вариант «Услуги связи», номер для теста 297777777)")
+    public void thrirdTest() {
+    driver.findElement(By.xpath(Locators.SERVICE_NAME)).getText();
+    driver.findElement(By.xpath(Locators.INPUT_NUMBER)).sendKeys("29777777");
+    driver.findElement(By.xpath(Locators.INPUT_SUMMA)).sendKeys("20");
+    driver.findElement(By.xpath(Locators.INPUT_EMAIL)).sendKeys("niknamed300@mail.ru");
+    driver.findElement(By.xpath(Locators.BUTTON_CON)).click();
+    driver.findElement(By.xpath(Locators.CARD_INFO)).isDisplayed();
 
-
-
-
+    }
 
     @AfterEach
     public void endTest() {
         driver.quit();
     }
 }
-
-
-
-
